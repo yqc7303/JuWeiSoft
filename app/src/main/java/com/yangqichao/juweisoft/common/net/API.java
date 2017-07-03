2283e3087ff2b2de4f65a1ce35e608ffcd889ebb
杨旗超 <793477679@qq.com>
2017-07-03 12:54:16 +0800

    Initial commit

---
+++ app/src/main/java/com/yangqichao/juweisoft/common/net/API.java
@@ -0,0 +1,66 @@
+package com.yangqichao.juweisoft.common.net;
+
+
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import rx.Observable;
+
+/**
+ * Created by yangqc on 2017/3/22.
+ */
+public interface API {
+
+//    @POST("app/login")
+//    Observable<Response<LoginBean>> login(@Body RequestBody requestBody);
+//
+//    @GET("app/searchWordQr/{id}/{userId}")
+//    Observable<Response<String>> searchWordQr(@Path("id") String id,
+//                                              @Path("userId") String userId);
+
+    @GET("app/searchPatentAppPic")
+    Observable<Response<String>> searchPatentAppPic();
+
+
+    @GET("app/user/check/{tel}")
+    Observable<Response<String>> check(@Path("tel") String tel);
+
+//    @POST("app/user/login")
+//    Observable<Response<LoginBean>> login(@Body RequestBody requestBody);
+//
+//    @GET("app/province/get")
+//    Observable<Response<List<LevelBean>>> get();
+//
+//    @GET("app/orgsub/get/{pid}")
+//    Observable<Response<List<LevelBean>>> getLevel(@Path("pid") String pid);
+//
+////    @POST("app/registe/add")
+////    Observable<Response<>> add(RequestBody requestBody);
+//
+//    @POST("/app/user/registe")
+//    Observable<Response<RegisteBean>> registe(@Body RequestBody requestBody);
+//
+//    @GET("/app/user/resetpassword/{tel}/{checkcode}")
+//    Observable<Response<String>> resetpassword(@Path("tel") String tel, @Path("checkcode") String checkcode);
+//
+//    @GET("/app/notifications/getbyuser/{userId}")
+//    Observable<Response<List<ScienceDynamicBean>>> getbyuser(@Path("userId") String userId);
+//
+//    @POST("/app/sharemsg/insertInfo")
+//    Observable<Response<String>> insertInfo(@Body RequestBody requestBody);
+//
+//    @POST("/app/org/users")
+//    Observable<Response<GetKeShiPerson>> users(@Body RequestBody requestBody);
+//
+//    @POST("/app/mymeetings/select")
+//    Observable<Response<MyMeetingBean>> select(@Body RequestBody requestBody);
+//
+//
+//    @GET("/app/meeting/get/{id}")
+//    Observable<Response<MeetingDetailBean>> meetingDetail(@Path("id") int id);
+//
+//    @GET("/app/meeting/sign/{meetingId}/{userId}/{gps}")
+//    Observable<Response<String>> sign(@Path("meetingId") String meetingId, @Path("userId") String userId, @Path("gps") String gps);
+//
+//    @GET("/app/meetingjoin/select/{meetingId}")
+//    Observable<Response<ShowPersonBean>> showPerson(@Path("meetingId") String meetingId);
+}
---
+++ app/src/main/java/com/yangqichao/juweisoft/common/net/RequestBody.java
@@ -0,0 +1,198 @@
+package com.yangqichao.juweisoft.common.net;
+
+/**
+ * Created by yangqc on 2017/3/23.
+ */
+
+public class RequestBody {
+
+
+    /**
+     * appType : 0
+     * cid : string
+     * loginName : string
+     * password : string
+     */
+
+    private String appType;
+    private String cid;
+    private String loginName;
+    private String password;
+    /**
+     * birthday : 2017-06-12T02:02:16.658Z
+     * checkCode : string
+     * deptId : 0
+     * deptName : string
+     * hospitalId : 0
+     * hospitalName : string
+     * name : string
+     * tel : string
+     */
+
+    private String birthday;
+    private String checkCode;
+    private String deptId;
+    private String deptName;
+    private String hospitalId;
+    private String hospitalName;
+    private String name;
+    private String tel;
+
+    private int page;
+    private int pageSize;
+    /**
+     * documentUrl : string
+     * shareContent : string
+     * shareUrl : string
+     * userId : 0
+     */
+
+    private String documentUrl;
+    private String shareContent;
+    private String shareUrl;
+    private String userId;
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public int getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public String getAppType() {
+        return appType;
+    }
+
+    public void setAppType(String appType) {
+        this.appType = appType;
+    }
+
+    public String getCid() {
+        return cid;
+    }
+
+    public void setCid(String cid) {
+        this.cid = cid;
+    }
+
+    public String getLoginName() {
+        return loginName;
+    }
+
+    public void setLoginName(String loginName) {
+        this.loginName = loginName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getCheckCode() {
+        return checkCode;
+    }
+
+    public void setCheckCode(String checkCode) {
+        this.checkCode = checkCode;
+    }
+
+    public String getDeptId() {
+        return deptId;
+    }
+
+    public void setDeptId(String deptId) {
+        this.deptId = deptId;
+    }
+
+    public String getDeptName() {
+        return deptName;
+    }
+
+    public void setDeptName(String deptName) {
+        this.deptName = deptName;
+    }
+
+    public String getHospitalId() {
+        return hospitalId;
+    }
+
+    public void setHospitalId(String hospitalId) {
+        this.hospitalId = hospitalId;
+    }
+
+    public String getHospitalName() {
+        return hospitalName;
+    }
+
+    public void setHospitalName(String hospitalName) {
+        this.hospitalName = hospitalName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getDocumentUrl() {
+        return documentUrl;
+    }
+
+    public void setDocumentUrl(String documentUrl) {
+        this.documentUrl = documentUrl;
+    }
+
+    public String getShareContent() {
+        return shareContent;
+    }
+
+    public void setShareContent(String shareContent) {
+        this.shareContent = shareContent;
+    }
+
+    public String getShareUrl() {
+        return shareUrl;
+    }
+
+    public void setShareUrl(String shareUrl) {
+        this.shareUrl = shareUrl;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+}
